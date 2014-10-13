eb862191eb353f5dca283b86fd799d5d5a5eeb99
Andriy Khomenko <andreyh@zeos.net>
2014-10-13 20:25:19 +0200

    Dev Update #1

--- js/geo-ws-model.js
+++ js/geo-ws-model.js
@@ -49,12 +49,17 @@ com.xomena.geo.Models.ParameterPart = Backbone.Model.extend({
 
 com.xomena.geo.Models.Instance = Backbone.Model.extend({
     defaults: {
-        webservices: null,
-        current_ws: null
+        id: null,
+        webservice: null,
+        version: null,
+        output: null,
+        parameters: null
     },
     validate: function(attrs, options){
     }
 });
+
+
 
 /*Define data collections*/
 com.xomena.geo.Collections.ParameterPartCollection = Backbone.Collection.extend({
@@ -74,6 +79,50 @@ com.xomena.geo.Collections.InstanceCollection = Backbone.Collection.extend({
 });
 
 com.xomena.geo.services = new com.xomena.geo.Collections.WebServiceCollection();
+
+/*Define views*/
+com.xomena.geo.Views.InstanceView = Backbone.View.extend({
+  tagName: 'li', // defaults to div if not specified
+  className: 'ws-instance', // optional, can also set multiple 
+  execInstance: function(){
+    console.log("Start execution for instance #"+this.model.get("id"));
+    return false;  
+  },
+  deleteInstance: function(){
+    console.log("Delete instance #"+this.model.get("id"));  
+    this.model.destroy(); // deletes the model when delete button clicked
+    return false;  
+  },    
+  events: {
+    'click .exec':   'execInstance',
+    'click .delete': 'deleteInstance'
+  },
+  newTemplate: _.template($('#instanceTemplate').html()), // external template    
+  initialize: function() {
+    this.render(); // render is an optional function that defines the logic for rendering a template
+    this.model.on('change', this.render, this); // calls render function once changed
+    this.model.on('destroy', this.remove, this); // calls remove function once model deleted  
+  },
+  render: function() {
+    this.$el.html(this.newTemplate(this.model.toJSON())); // calls the template
+  },
+  remove: function(){
+    this.$el.remove(); // removes the HTML element from view when delete button clicked/model deleted
+  }    
+});
+
+com.xomena.geo.Views.InstancesView = Backbone.View.extend({ 
+  tagName: 'ul',
+  initialize: function(){
+    this.collection;
+  },
+  render: function(){
+    this.collection.each(function(instance){
+      var instanceView = new com.xomena.geo.Views.InstanceView({model: instance});
+      $("#instances-container").append(instanceView.el);
+    });
+  }
+});
 
 
 (function($){
--- js/geo-ws.js
+++ js/geo-ws.js
@@ -2,34 +2,27 @@
     var API_KEY = "AIzaSyA67JIj41Ze0lbc2KidOgQMgqLOAZOcybE";
     var CLIENT_ID = "gme-addictive";
     
-    var services = [
-        {
-            name: "Geocoding API",
-            alias: "geocoding"
-        },
-        {
-            name: "Directions API",
-            alias: "directions"
-        },
-        {
-            name: "Distance Matrix API",
-            alias: "distancematrix"
-        },
-        {
-            name: "Elevation API",
-            alias: "elevation"
-        },
-        {
-            name: "Time Zone API",
-            alias: "timezone"
-        }
-    ];
+    function getNewId(){
+        getNewId.count = ++getNewId.count || 1;
+        return getNewId.count;
+    }
+    
+    var instance_col = new com.xomena.geo.Collections.InstanceCollection();
+    instance_col.add(new com.xomena.geo.Models.Instance({id: getNewId()}));
     
     //DOM ready initialization
     $(function(){
-       $.each(services, function(index,service){
-           $("<option value='"+service.alias+"'>"+service.name+"</option>").appendTo("#ws-choose");
-       }); 
+        // creates view for collection and renders collection
+        var instancesView = new com.xomena.geo.Views.InstancesView({collection: instance_col});
+        instancesView.render();
+        
+        //adding new instance
+        $("#add-instance").click(function(){
+            var m_instance = new com.xomena.geo.Models.Instance({id: getNewId()});
+            instance_col.add(m_instance);
+            var m_instanceView = new com.xomena.geo.Views.InstanceView({model: m_instance});
+            $("#instances-container").append(m_instanceView.el);
+            return false;
+        });
     });
-    
 })(jQuery);
